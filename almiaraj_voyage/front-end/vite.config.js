d333990c187ef1bf6ff5efcc0a0ae3d763cb46b2
Youssef El Archaoui <elarchaouiyoussef15@gmail.com>
2026-04-11 16:56:48 +0000

    Youssefbranch

--- almiaraj_voyage/front-end/vite.config.js
+++ almiaraj_voyage/front-end/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],  // فقط react، بدون tailwindcss
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
 })
